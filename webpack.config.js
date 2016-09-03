41dacd8317b22ddbe4a8fc9ee4ebf31fa917761f
Kenneth Powers <ken@kenpowers.net>
2016-09-03 02:41:10 -0400

    Add copyright banner.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const _ = require('lodash');
 const path = require('path');
+const webpack = require('webpack');
 const BabiliWebpackPlugin = require('babili-webpack-plugin');
+
+const version = require('./package.json').version;
+const year = (new Date()).getFullYear();
+const lodash = require('lodash/package.json');
+const webpackage = require('webpack/package.json');
 
 const baseConfig = {
   devtool: 'source-map',
@@ -21,6 +27,12 @@ const baseConfig = {
       loaders: ['babel'],
     }],
   },
+  plugins: [
+    new webpack.BannerPlugin(`Grumbles.js v${version} (C) ${year} Ken Powers (http://knpw.rs)
+Released under the MIT license.
+Contains parts of lodash v${lodash.version} under the ${lodash.license} license.
+Contains parts of webpack v${webpack.version} under the ${webpack.license} license.`),
+  ]
 };
 
 const minConfig = _.merge({}, baseConfig, {
@@ -29,6 +41,7 @@ const minConfig = _.merge({}, baseConfig, {
   },
   plugins: [
     new BabiliWebpackPlugin(),
+    new webpack.BannerPlugin(`Grumbles.js v${version} (C) ${year} MIT Ken Powers (http://knpw.rs) | Contains parts of lodash v${lodash.version} ${lodash.license} | Contains parts of webpack v${webpackage.version} ${webpackage.license}`),
   ]
 });
 
